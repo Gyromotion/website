d610040b5f2ab7fdc5906d1220d47acf9c74457c
Pratham <prathamnjoshi@gmail.com>
2024-08-07 15:19:00 +0530

    update to remove the .html

---
+++ common.js
@@ -0,0 +1,18 @@
+function loadHeader() {
+    const headerHTML = `
+        <header>
+            <a href="https://gyromotionphysio.in"><img src="img/logo-png.png" alt="Logo" class="logo" style="height: auto; max-width: 68%;"></a>
+        </header>
+        <nav>
+            <ul>
+                <li><a href="https://gyromotionphysio.in">Home</a></li>
+                <li><a href="about">About</a></li>
+                <li><a href="services.html">Services</a></li>
+                <li><a href="appointment.html">Book an Appointment</a></li>
+            </ul>
+        </nav>
+    `;
+    document.getElementById('common-header').innerHTML = headerHTML;
+}
+
+document.addEventListener('DOMContentLoaded', loadHeader);

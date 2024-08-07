029a7326e46b32806efc5a008774f6fa8f2eb836
Pratham <prathamnjoshi@gmail.com>
2024-08-07 15:36:30 +0530

    chnges to common nav

--- common.js
+++ common.js
@@ -6,9 +6,9 @@ function loadHeader() {
         <nav>
             <ul>
                 <li><a href="https://gyromotionphysio.in">Home</a></li>
-                <li><a href="about">About</a></li>
-                <li><a href="services">Services</a></li>
-                <li><a href="appointment">Book an Appointment</a></li>
+                <li><a href="about.html">About</a></li>
+                <li><a href="services.html">Services</a></li>
+                <li><a href="appointment.html">Book an Appointment</a></li>
             </ul>
         </nav>
     `;
